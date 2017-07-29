1524f4cab122e242f226588245afc1ed11b870bd
Bryce <bryce.sayers-kwan@live.ca>
2017-07-29 21:04:37 +0000

    Deleted schemas, impletemented class and contructor and fixed playlist functions

--- music-library.js
+++ music-library.js
@@ -1,47 +1,60 @@
-const librarySchema = {
-  name: 'Basic library',
-  creator: 'Kwan',
-  tracks: {},
-  playlists: []
-};
-
-const playlistSchema = {
-  name: 'Basic playlist',
-  tracks: [],
-  overallRating: function x() {},
-  totalDuration: function y() {}
-};
-
-const trackSchema = {
-  title: 'name',
-  rating: 1,
-  length: 123
-};
-
-function track(title, rating, length) {
-  this.title = title;
-  this.rating = rating;
-  this.length = length;
+class Track {
+  constructor(title, rating, length) {
+    this.title = title;
+    this.rating = rating;
+    this.length = length;
+  }
 }
 
-const newTrack = new track('Hypnotize', 5, 239);
-console.log(newTrack);
-const trackTwo = new track('Machine Gun Funk', 5, 261);
+const trackOne = new Track('Hypnotize', 5, 239);
+console.log(trackOne);
+const trackTwo = new Track('Machine Gun Funk', 5, 261);
 console.log(trackTwo);
 
-function playlist(name, tracks) {
-  this.name = name;
-  this.tracks = tracks;
+class Playlist {
+  constructor (name) {
+    this.name =  name;
+    this.tracks = [];
+  }
+  overallRating () {
+    return this.tracks.reduce((a, b) => {
+      return a.rating + b.rating;
+    });
+  }
+  totalDuration () {
+    return this.tracks.reduce((a, b) => {
+      return a.length + b.length;
+    });
+  }
 }
 
-const newPlaylist = new playlist('My Playlist', [newTrack, trackTwo]);
+const newPlaylist = new Playlist('My Playlist');
 console.log(newPlaylist);
 
-function library(name, creator, playlists) {
-  this.name = name;
-  this.creator = creator;
-  this.playlists = playlists;
+class Library {
+  constructor(name, creator) {
+    this.name = name;
+    this.creator = creator;
+    this.playlists = [];
+  }
 }
 
-const newLibrary = new library('Library', 'Bryce', [newPlaylist]);
+const newLibrary = new Library('Library', 'Bryce');
+console.log(newLibrary);
+
+function addTrackToPlaylist(track, playlist) {
+  playlist.tracks.push(track);
+}
+
+addTrackToPlaylist(trackOne, newPlaylist);
+addTrackToPlaylist(trackTwo, newPlaylist);
+console.log(newPlaylist);
+console.log(newPlaylist.overallRating());
+console.log(newPlaylist.totalDuration());
+
+function addPlaylist(library, playlist) {
+  library.playlists.push(playlist);
+}
+
+addPlaylist(newLibrary, newPlaylist);
 console.log(newLibrary);
