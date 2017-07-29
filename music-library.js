9ee128441b8b8eff04c2bc7ea2e58576f8f44377
Bryce <bryce.sayers-kwan@live.ca>
2017-07-29 02:58:05 +0000

    Initial commit with obj skeletons

---
+++ music-library.js
@@ -0,0 +1,20 @@
+const library = {
+  name: 'Basic library',
+  creator: 'Kwan',
+  tracks: {},
+  playlists: []
+};
+
+const playlist = {
+  name: 'Basic playlist',
+  tracks: [],
+  overallRating: function x() {},
+  totalDuration: function y() {}
+};
+
+const track = {
+  title: 'name',
+  rating: 1,
+  length: 123
+};
+
