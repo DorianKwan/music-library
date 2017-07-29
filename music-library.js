24692896a66f33f09df634dcd063ab2b1101f3e7
Bryce <bryce.sayers-kwan@live.ca>
2017-07-29 03:58:50 +0000

    Added functions to create tracks, playlists and libraries

--- music-library.js
+++ music-library.js
@@ -1,20 +1,47 @@
-const library = {
+const librarySchema = {
   name: 'Basic library',
   creator: 'Kwan',
   tracks: {},
   playlists: []
 };
 
-const playlist = {
+const playlistSchema = {
   name: 'Basic playlist',
   tracks: [],
   overallRating: function x() {},
   totalDuration: function y() {}
 };
 
-const track = {
+const trackSchema = {
   title: 'name',
   rating: 1,
   length: 123
 };
 
+function track(title, rating, length) {
+  this.title = title;
+  this.rating = rating;
+  this.length = length;
+}
+
+const newTrack = new track('Hypnotize', 5, 239);
+console.log(newTrack);
+const trackTwo = new track('Machine Gun Funk', 5, 261);
+console.log(trackTwo);
+
+function playlist(name, tracks) {
+  this.name = name;
+  this.tracks = tracks;
+}
+
+const newPlaylist = new playlist('My Playlist', [newTrack, trackTwo]);
+console.log(newPlaylist);
+
+function library(name, creator, playlists) {
+  this.name = name;
+  this.creator = creator;
+  this.playlists = playlists;
+}
+
+const newLibrary = new library('Library', 'Bryce', [newPlaylist]);
+console.log(newLibrary);
